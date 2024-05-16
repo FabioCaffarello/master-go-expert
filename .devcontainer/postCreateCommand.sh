2b258207843270aabb6f892ee7291f6f8189287d
FabioCaffarello <fabio.caffarello@gmail.com>
2024-05-16 18:22:49 -0300

    feat: code review

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,5 +3,8 @@
 # Setting node
 npm install || { echo "npm install failed"; exit 1; }
 
+# Setting husky
+npm run prepare || { echo "npm run prepare failed"; exit 1; }
+
 # Setting golang
 go install github.com/google/wire/cmd/wire@latest || { echo "go install failed"; exit 1; }
