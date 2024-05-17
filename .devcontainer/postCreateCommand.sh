6e7768da91e5c5ffd57a5cc8d443d3bec252a2e4
Fabio Caffarello <fabio.caffarello@gmail.com>
2024-05-17 00:45:28 +0000

    feat: add go doc to markdown

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,4 +7,7 @@ npm install || { echo "npm install failed"; exit 1; }
 npm run prepare || { echo "npm run prepare failed"; exit 1; }
 
 # Setting golang
+## Dependency Injection
 go install github.com/google/wire/cmd/wire@latest || { echo "go install failed"; exit 1; }
+## Documentation
+go install github.com/princjef/gomarkdoc/cmd/gomarkdoc@latest
