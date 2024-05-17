e0bf6b5146fcb57586b9193e1e2e71b1749ea100
FabioCaffarello <fabio.caffarello@gmail.com>
2024-05-16 21:07:34 -0300

    feat: add tools

--- libs/shared/go-uuid/uuid.go
+++ libs/shared/go-uuid/uuid.go
@@ -10,6 +10,9 @@ import (
 
 type ID = string
 
+// GetID generates a unique ID based on the provided properties.
+// It serializes the properties to JSON, hashes the resulting byte slice, and generates a UUID from the hash.
+// Returns the generated ID and any error encountered during the process.
 func GetID(properties map[string]interface{}) (string, error) {
 	serializedConfigProperties, err := json.Marshal(properties)
 	if err != nil {
@@ -22,11 +25,16 @@ func GetID(properties map[string]interface{}) (string, error) {
 	return ID(configID), nil
 }
 
+// hashConfig computes the SHA-256 hash of the given data.
+// Returns the hash as a byte slice.
 func hashConfig(data []byte) []byte {
 	hash := sha256.Sum256(data)
 	return hash[:]
 }
 
+// generateUUIDFromHash generates a UUID based on the provided hash.
+// It computes a second SHA-256 hash of the input hash and then generates a UUID using the SHA-1 algorithm.
+// Returns the generated UUID as a string.
 func generateUUIDFromHash(hash []byte) string {
 	combinedHash := sha256.Sum256(hash)
 	return uuid.NewSHA1(uuid.Nil, combinedHash[:]).String()
